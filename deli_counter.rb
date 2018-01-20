7d06b1f0cd90a76a41fa3037538e45c682ac9e9b
Catherine Salazar <catcreates13@gmail.com>
2018-01-20 04:10:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ def now_serving(katz_deli)
   if katz_deli.empty?
   puts  "There is nobody waiting to be served!"
 else
-  katz_deli.each do |deli|
+  katz_deli.shift
 
   puts  "Currently serving #{person}"
 end
